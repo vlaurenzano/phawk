713e32b267b8ed07b45eb696e2cf5e68b4f77954
vlaurenzano <vincent.laurenzano@gmail.com>
2016-12-21 20:13:18 -0500

    test travis test theory

--- tests/tests.php
+++ tests/tests.php
@@ -14,44 +14,44 @@ foreach(get_defined_functions()['user'] as $func){
 function test_pipe_simple_file(){        
     global $phawk;
     global $simpleTestFile;            
-    $result = shell_exec("cat $simpleTestFile | $phawk '" . 'echo $v1, $v2;' . "'");
+    $result = shell_exec("cat $simpleTestFile | php $phawk '" . 'echo $v1, $v2;' . "'");
     simple_assert($result === '1256ABEFIJMN');            
 }
 
 function test_print_line_numbers(){
     global $phawk;
     global $simpleTestFile;        
-    $result = shell_exec("cat $simpleTestFile | $phawk '" . 'echo $ln;' . "'");    
+    $result = shell_exec("cat $simpleTestFile | php $phawk '" . 'echo $ln;' . "'");    
     simple_assert($result === '123456');            
 }
 
 function test_bool_functionality(){
     global $phawk;
     global $simpleTestFile;        
-    $result = shell_exec("cat $simpleTestFile | $phawk '" . '$display = $ln %2 ===0;' . "'");    
+    $result = shell_exec("cat $simpleTestFile | php $phawk '" . '$display = $ln %2 ===0;' . "'");    
     simple_assert(substr_count($result, "\n") === 3);            
 }
 
 function test_begin_end_functionality() {
     global $phawk;
     global $simpleTestFile;        
-    $result = shell_exec("cat $simpleTestFile | $phawk " . '\'$myVar = 0;\' \'$myVar++;$display = false;\' \'echo $myVar;\'' );        
+    $result = shell_exec("cat $simpleTestFile | php $phawk " . '\'$myVar = 0;\' \'$myVar++;$display = false;\' \'echo $myVar;\'' );        
     simple_assert($result == 6);            
 }
 
 function test_wrong_number_arguments() {
     global $phawk;
     global $simpleTestFile;        
-    $result = shell_exec("cat $simpleTestFile | $phawk " . '\'$myVar = 0;\' \'$myVar++;$display = true;\'' );                
+    $result = shell_exec("cat $simpleTestFile | php $phawk " . '\'$myVar = 0;\' \'$myVar++;$display = true;\'' );                
     simple_assert(!$result);            
 }
 
 function test_field_seperators() {
     global $phawk;
     $pattern = "'test,test,test,test'";
-    $result = shell_exec("echo $pattern | $phawk " . '\'echo $v1;\' \',\'' );            
+    $result = shell_exec("echo $pattern | php $phawk " . '\'echo $v1;\' \',\'' );            
     simple_assert($result === 'test');                
-    $result = shell_exec("echo $pattern | $phawk " . '\'$myvar=1;\' \'echo $v1;\' \'$myvar=2;\' \',\'' );                    
+    $result = shell_exec("echo $pattern | php $phawk " . '\'$myvar=1;\' \'echo $v1;\' \'$myvar=2;\' \',\'' );                    
     simple_assert($result === 'test');            
 }
 
